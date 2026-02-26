2fd19a641fcd5a46caa1ae69a7151f363321696f
Shtt4r <sbmaten@gmail.com>
2026-02-26 05:07:31 +0200

    Add files via upload

---
+++ gambling_gaem.py
@@ -0,0 +1,31 @@
+import random
+
+money = 100
+
+while money < 3193894:
+ ask = input("black or red:")
+
+ print(f"you have {money}")
+ try:
+  bet = int(input("how much to bet:"))
+ except ValueError:
+    print("trying to break the game huh?")
+    continue
+
+
+ roll = random.randint(0, 1)
+
+ if roll == 0:
+       roll = "black"
+
+ else:
+     roll = "red"
+
+ if ask == roll:
+
+     money = money + bet
+     print(f"win {money}") 
+ else: 
+     
+     money = money - bet
+     print(f"lose {money}")
